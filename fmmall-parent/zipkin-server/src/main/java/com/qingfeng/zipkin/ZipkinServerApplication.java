a8ed4480b1a6b36e5a65b71cd261b47e83100b6c
pengjialong <2562777581@qq.com>
2022-03-06 11:00:13 +0800

    微服务配置

---
+++ fmmall-parent/zipkin-server/src/main/java/com/qingfeng/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,18 @@
+package com.qingfeng.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+/**
+ * @author 清风学Java
+ */
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+
+}
